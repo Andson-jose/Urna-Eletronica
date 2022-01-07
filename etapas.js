1154fd6717b0e9f9fcd869a8746fe0d169270fba
Andson Wagner <andson.wagner@comeialabs.com.br>
2022-01-07 15:25:33 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,45 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                nome: 'Wilian Rockfeler',
+                partido: 'QQQ',
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Natanie bronx',
+                partido: 'WEE',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '20',
+                nome: 'Bill Gates',
+                partido: 'QQQ',
+                fotos: [
+                    { url: 'bill.jpg', legenda: 'prefeito' },
+                    { url: 'tom.jpg', legenda: 'vice-prefeito', small: true }
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Arnold Schwarzenegger',
+                partido: 'WEE',
+                fotos: [
+                    { url: 'Arnold1.jpg', legenda: 'prefeito' },
+                    { url: 'trup.jpg', legenda: 'vice-prefeito', small: true }
+                ]
+            },
+        ]
+    },
+]
